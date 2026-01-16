9d1a005554fa3d3057d06edf8abfc725ba39dffc
NikileshJayaguptha <Njayaguptha@gmail.com>
2026-01-16 17:28:30 +0530

    registration done

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -31,7 +31,7 @@ function Navbar() {
         <li onClick={() => setOpen(false)}><Link to="/schedule">Schedule</Link></li>
         <li onClick={() => setOpen(false)}><Link to="/programs">Programs</Link></li>
         <li onClick={() => setOpen(false)}><Link to="/commitee">Committee</Link></li>
-        <li onClick={() => setOpen(false)}><Link to="/registration">Registration</Link></li>
+        <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSfcuNDjHwn4SHuMOq5bJTNRc3P3fBTNb7H32dttJjs49qaanw/viewform?usp=publish-editor">Registration</a></li>
       </ul>
     </nav>
   )
